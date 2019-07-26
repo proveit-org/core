9ccce0aaf1e569525a0e35e8904028b06be30100
Emma Hsueh <ehsueh1993@gmail.com>
2019-07-27 00:44:52 +0800

    fixing location issue; trigger cloud build to test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,40 @@
 // tslint:disable-next-line: no-import-side-effect
 import 'source-map-support/register'
-import { config, https, pubsub } from 'firebase-functions';
+import { config } from 'firebase-functions';
 
 const admin = require('firebase-admin');
 admin.initializeApp(config().firebase);
 
 // API
 
-// Add to collection
+// // Add to collection
+// import { storeApp } from './api/store';
+// export const store = https.onRequest(storeApp)
+
+// // Retrieve proof 
+// import { proveApp } from './api/prove';
+// export const prove = https.onRequest(proveApp);
+
+// // Download file
+// import { downloadApp } from './api/download';
+// export const download = https.onRequest(downloadApp);
+
+// // Process item collections and make a merkle tree
+// import { work } from './workers/metaverse-testnet';
+// export const mvsWorker = pubsub.schedule('every 3 minutes')
+//     .timeZone('America/New_York')
+//     .onRun(work);
+
+const functions = require('firebase-functions')
+
 import { storeApp } from './api/store';
-export const store = https.onRequest(storeApp)
+exports.store = functions.region('asia-northeast1').https.onRequest(storeApp);
 
-// Retrieve proof 
 import { proveApp } from './api/prove';
-export const prove = https.onRequest(proveApp);
+exports.prove = functions.region('asia-northeast1').https.onRequest(proveApp);
 
-// Download file
 import { downloadApp } from './api/download';
-export const download = https.onRequest(downloadApp);
+exports.download = functions.region('asia-northeast1').https.onRequest(downloadApp);
 
-// Process item collections and make a merkle tree
 import { work } from './workers/metaverse-testnet';
-export const mvsWorker = pubsub.schedule('every 3 minutes')
-    .timeZone('America/New_York')
-    .onRun(work);
-
+exports.mvsWorker = functions.region('asia-northeast1').pubsub.schedule('every 3 minutes').timeZone('America/New_York').onRun(work);
